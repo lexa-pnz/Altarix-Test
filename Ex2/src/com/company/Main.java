5ee72c26ea8f176b6caa633d6d4ecd4a659bd9ab
lexa-pnz <33934909+lexa-pnz@users.noreply.github.com>
2018-03-15 20:20:27 +0300

    2.0

--- Ex2/src/com/company/Main.java
+++ Ex2/src/com/company/Main.java
@@ -6,52 +6,71 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите размер кредита: ");
-        double S = scanner.nextDouble(); //Размер кредита
-
-        System.out.print("Введите количество меяцев: ");
-        int N = scanner.nextInt(); //Месяц
-        System.out.print("\n\n");
-
-        double P = 12; //Проценты
+        double P=12; //Проценты
         double x; //Размер платежа
+        double S; //Размер кредита
+        int N; //Кол-во месяцев
         double st,mps,mps2,pst;
 
+        Scanner scanner = new Scanner(System.in);
 
-        mps = P/100/12;
-        mps2 = mps + 1;
-        st = Math.pow(mps2, -N);
-        x = S * (mps / (1 - st));
+        System.out.print("Введите размер кредита: ");
+        S = scanner.nextDouble();
+        System.out.print("\n");
+
+        int a;
+        for (;;) {
+            System.out.print("Введите кол-во месяцев: ");
+            N = scanner.nextInt();
+
+            mps = P/100/12;
+            mps2 = mps + 1;
+            st = Math.pow(mps2, -N);
+            x = S * (mps / (1 - st));
+            System.out.println("Ежемесячный платеж = " + Math.round(x * 100.0) / 100.0 );
+
+            System.out.print("Вас устраивает ежемесячный платеж? (1 - Yes / 0 - No)");
+            a = scanner.nextInt();
+            if (a == 1)
+                break;
+        }
+        System.out.print("\n");
+
+        System.out.println("Сумма кредита: " + Math.round(S * 100.0) / 100.0 + " руб.");
+        System.out.println("Срок кредитования: " + N + " мес.");
+        System.out.println("Общий ежемесячный платеж: " + + Math.round(x * 100.0) / 100.0 + " руб.");
+
+
+        System.out.print("\n");
 
         int mes=1;
         double pl ;
-        System.out.print("Номер месяца\t");
-        System.out.print("Платеж в счет\t");
-        System.out.print("Проценты по кредиту\t");
-        System.out.print("Общий ежемесячный \t");
-        System.out.print("Остаток осн. долга после \t");
-        System.out.print("\n\t\t\t\tпогашения \t");
-        System.out.print("\t\t\t\t\t\t\tплатеж \t");
-        System.out.print("\t\tсовершения текущего платежа \t");
-        System.out.print("\n\t\t\t\tосновного долга \t");
-
-        do {
+        double dosr;
+        while (S > 0.1){
             System.out.println(" ");
             System.out.println(" ");
-            if (mes < 10)
-                System.out.print(mes + " Месяц " + "\t\t");
-            else
-                System.out.print(mes + " Месяц" + "\t\t");
+            System.out.print(mes + " Месяц\n");
             mes++;
+
+            System.out.print("Введите размер досрочного платежа (сверх ежемесячного платежа): ");
+            dosr = scanner.nextDouble();
+
             pst = S * mps;
             pl = x - pst;
             S = S - x + pst;
-            System.out.print(Math.round(pl * 100.0) / 100.0 + "\t\t\t\t");
-            System.out.print(Math.round(pst * 100.0) / 100.0 + "\t\t\t\t");
-            System.out.print(Math.round(x * 100.0) / 100.0 + "\t\t\t\t");
-            System.out.print(Math.round(S * 100.0) / 100.0 + "\t\t\t\t");
+            S = S - dosr;
+            System.out.println("Платеж в счет погашения основного долга, руб\t\t\t\t\t" + Math.round(pl * 100.0) / 100.0 );
+            System.out.println("Проценты по кредиту, руб\t\t\t\t\t\t\t\t\t\t" + Math.round(pst * 100.0) / 100.0 );
+            System.out.println("Общий ежемесячный платеж, руб\t\t\t\t\t\t\t\t\t" + Math.round(x * 100.0) / 100.0 );
+            System.out.println("Остаток осн. долга после совершения текущего платежа, руб\t\t" + Math.round(S * 100.0) / 100.0 );
+            System.out.println("Досрочное погашение (сверх ежемесячного платежа), руб\t\t\t" + Math.round(dosr * 100.0) / 100.0 );
+
+            if(dosr != 0){
+                mps = P/100/12;
+                mps2 = mps + 1;
+                st = Math.pow(mps2, -N + mes -1);
+                x = S * (mps / (1 - st));
+            }
         }
-        while (S > 1);
     }
 }
